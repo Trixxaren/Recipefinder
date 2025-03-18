38137d412b1eba6d31b325aad0e34ad978d93525
Robin Vikström <robin.m.e.vikstrom@gmail.com>
2025-03-18 18:41:41 +0100

    sidebar klar till både mobil & desktop

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ const Sidebar = () => {
   return (
     <>
       <DesktopSidebar />
-      {/* <MobileSidebar /> */}
+      <MobileSidebar />
     </>
   );
 };
@@ -35,6 +35,22 @@ const DesktopSidebar = () => {
           </li>
         </ul>
       </div>
+    </div>
+  );
+};
+
+const MobileSidebar = () => {
+  return (
+    <div
+      className="flex justify-center gap-10 border-t fixed w-full
+    bottom-0 left-0 bg-white z-10 p-2 sm:hidden"
+    >
+      <Link to={"/"}>
+        <Home size={"24"} className="cursor-pointer" />
+      </Link>
+      <Link to={"/favorites"}>
+        <Heart size={"24"} className="cursor-pointer" />
+      </Link>
     </div>
   );
 };
